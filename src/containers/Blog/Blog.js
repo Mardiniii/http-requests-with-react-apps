899ddb900fde5b3b0fcf63c8bab63b0f388f0ba3
mardiniii <sezama03@hotmail.com>
2020-10-05 09:17:56 -0400

    174. Creating and Using Axios Instances

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
+import axiosInstance from "../../../src/axios";
 
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
@@ -14,8 +15,8 @@ class Blog extends Component {
   };
 
   componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+    axiosInstance
+      .get("/posts")
       .then((response) => {
         const posts = response.data.slice(0, 4);
         const updatedPosts = posts.map((post) => {
